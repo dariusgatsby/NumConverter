32bc794bd7e5d362406c276f88b718fbd4f5fb97
dariusgatsby <145069350+dariusgatsby@users.noreply.github.com>
2023-12-22 02:40:27 -0800

    added exception handling

--- NumConverterGUI.py
+++ NumConverterGUI.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 from convert_function import convert
 
+sg.theme('Black')
 feet_label = sg.Text("Enter feet:")
 inches_label = sg.Text("Enter inches:")
 output = sg.Text('', key='output', text_color='black')
@@ -9,16 +10,26 @@ input_box1 = sg.InputText(key='feet')
 input_box2 = sg.InputText(key='inches')
 
 convert_button = sg.Button("Convert")
+exit_button = sg.Button("Exit")
 
 layout = [[feet_label, input_box1],
           [inches_label, input_box2],
-          [convert_button, output]]
-window = sg.Window("Converter", layout=layout)
+          [convert_button, exit_button, output]]
+window = sg.Window("Converter", layout=layout,)
 
 while True:
     events, values = window.read()
+    match events:
+        case 'Exit':
+            break
+        case sg.WIN_CLOSED:
+            break
+    try:
+        meters = convert(values['feet'], values['inches'])
+        window['output'].update(value=meters)
 
-    meters = convert(values['feet'], values['inches'])
-    window['output'].update(value=meters)
+    except ValueError:
+        sg.popup_ok("Please enter two values")
+        continue
 
 window.close()
